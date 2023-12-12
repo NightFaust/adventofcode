db6872b296cd7ecdd647950fbb3235e4d7717cbc
lseguin <lseguin@activeassurances.fr>
2023-12-12 21:45:01 +0100

    10-2023 Part 2

--- 2023/Day10/Solution.cs
+++ 2023/Day10/Solution.cs
@@ -15,12 +15,12 @@ class Solution : Solver
         // We find where the 'S' is
         var startingPoint = FindStartingPoint(lines);
         pipesMap.Add(startingPoint.Key, startingPoint.Value);
-        
+
         while (true)
         {
             var currentPoint = new ValueTuple<int, int>();
             var newDirection = Direction.Down;
-        
+
             if (pipesMap.Last().Value == Direction.Right)
             {
                 currentPoint = (pipesMap.Last().Key.Item1, pipesMap.Last().Key.Item2 + 1);
@@ -41,22 +41,104 @@ class Solution : Solver
                 currentPoint = (pipesMap.Last().Key.Item1 + 1, pipesMap.Last().Key.Item2);
                 newDirection = GetNewDirection(Direction.Down, lines[currentPoint.Item1][currentPoint.Item2]);
             }
-        
+
             if (newDirection == Direction.End)
             {
                 // We went back to the previous point
                 break;
             }
-            
+
             pipesMap.Add(currentPoint, newDirection);
         }
-        
+
         return (pipesMap.Count) / 2;
     }
 
     public object PartTwo(string input)
     {
-        return 0;
+        var lines = ParseLines(input);
+        var pipesMap = new Dictionary<(int, int), Direction>();
+        var visitedTiles = new HashSet<(int, int)>();
+        // We find where the 'S' is
+        var startingPoint = FindStartingPoint(lines);
+
+        pipesMap.Add(startingPoint.Key, startingPoint.Value);
+        visitedTiles.Add(startingPoint.Key);
+
+        while (true)
+        {
+            var currentPoint = new ValueTuple<int, int>();
+            var newDirection = Direction.Down;
+
+            if (pipesMap.Last().Value == Direction.Right)
+            {
+                currentPoint = (pipesMap.Last().Key.Item1, pipesMap.Last().Key.Item2 + 1);
+                newDirection = GetNewDirection(Direction.Right, lines[currentPoint.Item1][currentPoint.Item2]);
+            }
+            else if (pipesMap.Last().Value == Direction.Left)
+            {
+                currentPoint = (pipesMap.Last().Key.Item1, pipesMap.Last().Key.Item2 - 1);
+                newDirection = GetNewDirection(Direction.Left, lines[currentPoint.Item1][currentPoint.Item2]);
+            }
+            else if (pipesMap.Last().Value == Direction.Up)
+            {
+                currentPoint = (pipesMap.Last().Key.Item1 - 1, pipesMap.Last().Key.Item2);
+                newDirection = GetNewDirection(Direction.Up, lines[currentPoint.Item1][currentPoint.Item2]);
+            }
+            else if (pipesMap.Last().Value == Direction.Down)
+            {
+                currentPoint = (pipesMap.Last().Key.Item1 + 1, pipesMap.Last().Key.Item2);
+                newDirection = GetNewDirection(Direction.Down, lines[currentPoint.Item1][currentPoint.Item2]);
+            }
+
+            if (newDirection == Direction.End)
+            {
+                // We went back to the previous point
+                break;
+            }
+
+            pipesMap.Add(currentPoint, newDirection);
+            visitedTiles.Add(currentPoint);
+        }
+
+        // We modify Starting point for algo
+        var index = startingPoint.Key.Item2;
+        var str = lines[startingPoint.Key.Item1];
+        var newStr = str.Remove(index, 1).Insert(index, "L");
+        lines[startingPoint.Key.Item1] = newStr;
+
+        var insideCount = 0;
+        for (int y = 1; y < lines.Length - 1; y++)
+        {
+            for (int x = 1; x < lines[0].Length - 1; x++)
+            {
+                if (visitedTiles.Contains((y, x)))
+                {
+                    continue;
+                }
+
+                var crossings = 0;
+
+                for (int i = x + 1; i < lines[0].Length; i++)
+                {
+                    var tile = lines[y][i];
+                    // If we find a wall
+                    var symbols = new[] { '|', 'J', 'L' };
+
+                    if (visitedTiles.Contains((y, i)) && symbols.Contains(tile))
+                    {
+                        crossings++;
+                    }
+                }
+
+                if (crossings % 2 == 1)
+                {
+                    insideCount++;
+                }
+            }
+        }
+
+        return insideCount;
     }
 
     private static string[] ParseLines(string input) =>
