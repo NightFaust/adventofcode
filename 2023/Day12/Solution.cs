5ba3bddad80811d8a184490409b8f8a31ace6d86
lseguin <lseguin@activeassurances.fr>
2023-12-18 22:43:06 +0100

    12-2023 Part 2 + Optimization

--- 2023/Day12/Solution.cs
+++ 2023/Day12/Solution.cs
@@ -1,93 +1,106 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
-using AngleSharp.Media.Dom;
 
 namespace AdventOfCode.Y2023.Day12;
 
 [ProblemName("Hot Springs")]
 class Solution : Solver
 {
+    private readonly Dictionary<(long, long, long), long> dp = new();
+
     public object PartOne(string input)
     {
-        var lines = ParseLines(input);
-        var answer = 0;
-
-        foreach (var line in lines)
+        long answer = 0;
+        foreach (var line in ParseLines(input))
         {
             var splits = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
             var dots = splits[0];
             var numbers = splits[1];
-
             var blocks = numbers.Split(",")
-                .Select(int.Parse);
-
-            var score = Resolve(dots, blocks, 0);
+                .Select(int.Parse)
+                .ToList();
+            
+            dp.Clear();
+            
+            var score = Resolve(dots, blocks, 0, 0, 0);
             answer += score;
         }
-        
+
         return answer;
     }
 
     public object PartTwo(string input)
     {
-        var lines = ParseLines(input);
-        return 0;
+        long answer = 0;
+        foreach (var line in ParseLines(input))
+        {
+            var splits = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var dots = string.Join("?", Enumerable.Repeat(splits[0], 5));
+            var numbers = string.Join(",", Enumerable.Repeat(splits[1], 5));
+            var blocks = numbers.Split(",")
+                .Select(int.Parse)
+                .ToList();
+            
+            dp.Clear();
+            
+            var score = Resolve(dots, blocks, 0, 0, 0);
+            answer += score;
+        }
+
+        return answer;
     }
 
     private static string[] ParseLines(string input) =>
         input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
-    
-    private static int Resolve(string dots, IEnumerable<int> blocks, int index)
+
+    private long Resolve(string dots, IReadOnlyList<int> blocks, int index, int blockIndex, long current)
     {
+        var key = (index, blockIndex, current);
+
+        if (dp.TryGetValue(key, out long value))
+        {
+            return value;
+        }
+
         if (index == dots.Length)
         {
-            return IsValid(dots, blocks) ? 1 : 0;
-        }
-
-        if (dots[index] == '?')
-        {
-            return Resolve($"{dots[..index]}#{dots[(index+1)..]}", blocks, index+1) +
-                   Resolve($"{dots[..index]}.{dots[(index+1)..]}", blocks, index+1);
-        }
-
-        return Resolve(dots, blocks, index+1);
-    }
-
-    private static bool IsValid(string dots, IEnumerable<int> blocks)
-    {
-        var current = 0;
-        List<int> seen = [];
-
-        foreach (var dot in dots)
-        {
-            if (dot == '.')
+            if (blockIndex == blocks.Count && current == 0)
             {
-                if (current > 0)
+                return 1;
+            }
+
+            if (blockIndex == blocks.Count - 1 && blocks[(int)blockIndex] == current)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        long answer = 0;
+
+        foreach (var c in new[] { '.', '#' })
+        {
+            if (dots[(int)index] == c || dots[(int)index] == '?')
+            {
+                if (c == '.' && current == 0)
                 {
-                    seen.Add(current);
+                    answer += Resolve(dots, blocks, index + 1, blockIndex, 0);
                 }
-
-                current = 0;
-            }
-            else if (dot == '#')
-            {
-                current++;
-            }
-            else
-            {
-                throw new Exception("Invalid dot");
+                else if (c == '.' && current > 0 && blockIndex < blocks.Count() && blocks[(int)blockIndex] == current)
+                {
+                    answer += Resolve(dots, blocks, index + 1, blockIndex + 1, 0);
+                }
+                else if (c == '#')
+                {
+                    answer += Resolve(dots, blocks, index + 1, blockIndex, current + 1);
+                }
             }
         }
 
-        if (current > 0)
-        {
-            seen.Add(current);
-        }
-
-        return seen.SequenceEqual(blocks);
+        dp.Add(key, answer);
+        
+        return answer;
     }
 }
